0027a4309e4ffae990f57fcc70a7f2622419f0c2
tjtaill36@gmail.com <tjtaill36@gmail.com>
2015-12-10 17:59:56 -0500

    update changes

--- trunk/src/plantplugin/PlantFoldHandler.java
+++ trunk/src/plantplugin/PlantFoldHandler.java
@@ -4,25 +4,33 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.gjt.sp.jedit.EBComponent;
+import org.gjt.sp.jedit.EBMessage;
+import org.gjt.sp.jedit.EditBus;
 import org.gjt.sp.jedit.buffer.FoldHandler;
 import org.gjt.sp.jedit.buffer.JEditBuffer;
+import org.gjt.sp.jedit.msg.EditPaneUpdate;
 import plantparser.PlantumlLexer;
 import plantparser.PlantumlParser;
 
 import javax.swing.text.Segment;
 import java.util.HashMap;
 
-public class PlantFoldHandler extends FoldHandler {
+public class PlantFoldHandler extends FoldHandler implements EBComponent {
     private boolean needsParsing = true;
     private HashMap<Integer, Integer> foldLevels = new HashMap<>();
+    private JEditBuffer buffer;
 
     public PlantFoldHandler() {
         super("plant");
+        EditBus.addToBus(this);
     }
 
     @Override
     public int getFoldLevel(JEditBuffer jEditBuffer, int i, Segment segment) {
         if (needsParsing) {
+            buffer = jEditBuffer;
+            foldLevels.clear();
             try {
                 PlantumlLexer lexer = new PlantumlLexer(new ANTLRInputStream(jEditBuffer.getText()));
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -47,4 +55,14 @@ public class PlantFoldHandler extends FoldHandler {
     }
 
 
+    @Override
+    public void handleMessage(EBMessage ebMessage) {
+        if( ebMessage instanceof EditPaneUpdate) {
+            EditPaneUpdate editPaneUpdate = (EditPaneUpdate) ebMessage;
+
+            if ( !needsParsing && editPaneUpdate.getSource() == buffer && editPaneUpdate.getWhat() == EditPaneUpdate.BUFFER_CHANGED ) {
+                needsParsing = true;
+            }
+        }
+    }
 }
